d312156c6ad05e93aab9913043e6dd77c633c50a
yz <yz@yz.com>
2020-01-08 19:22:18 +0800

    配置MySQL数据库

--- DocumentManageSystem/DocumentManageSystem/settings.py
+++ DocumentManageSystem/DocumentManageSystem/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'DocumentManageSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '39.106.224.193',
+        'PORT': 3306,
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'NAME': 'DocManage'
     }
 }
 
