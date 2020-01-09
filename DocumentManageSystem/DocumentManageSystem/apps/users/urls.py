c727295017a8c19e1d971763ade87c61e50a9145
yz <yz@yz.com>
2020-01-09 19:54:00 +0800

    屏蔽CSRF，初步完成创建单一用户接口功能

--- DocumentManageSystem/DocumentManageSystem/apps/users/urls.py
+++ DocumentManageSystem/DocumentManageSystem/apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from . import views
+
 # Register your models here.
 
 urlpatterns = [
-
+    path('create/', views.CreateUserView.as_view())
 ]
--- DocumentManageSystem/DocumentManageSystem/apps/users/views.py
+++ DocumentManageSystem/DocumentManageSystem/apps/users/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views import View
+from django import http
+
+from users.models import UserModel
+
 
 # Create your views here.
+
+
+class CreateUserView(View):
+    """创建用户"""
+
+    def post(self, request):
+        # 接收参数
+        try:
+            index = request.POST['index']
+            username = request.POST['username']
+            password = request.POST['password']
+            name = request.POST['name']
+            sex = request.POST['sex']
+            role = request.POST['role']
+        except:
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 创建用户
+        try:
+            UserModel.objects.create_user(
+                index=index,
+                username=username,
+                password=password,
+                name=name,
+                sex=sex,
+                role=role
+            )
+        except:
+            return http.JsonResponse({'msg': 'ERROR'})
+
+        # 返回响应
+        return http.JsonResponse({'msg': 'OK'})
