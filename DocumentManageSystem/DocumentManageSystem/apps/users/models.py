fed985ce96af8e0a36c99638d109c8cecc0d1f3d
yz <yz@yz.com>
2020-01-10 19:22:34 +0800

    设计学生模型

--- DocumentManageSystem/DocumentManageSystem/apps/users/models.py
+++ DocumentManageSystem/DocumentManageSystem/apps/users/models.py
@@ -4,6 +4,27 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class StudentModel(models.Model):
+    """学生模型"""
+    # 学号
+    index = models.CharField(max_length=12, null=False)
+    # 年级
+    grade = models.IntegerField(null=False)
+    # 院系
+    college = models.CharField(max_length=50, null=False)
+    # 专业
+    major = models.CharField(max_length=50, null=False)
+    # 班级
+    classes = models.IntegerField(null=False)
+    # 负责教师工号
+    teacher = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = 'tb_student'
+        verbose_name = '学生表'
+        verbose_name_plural = verbose_name
+
+
 class UserModel(AbstractUser):
     """用户模型"""
     # 姓名
